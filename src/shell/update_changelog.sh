18d27552b90859eee2a30991c1ef927eedd02943
François CABANNES <cabannes.francois.danse@gmail.com>
2025-02-24 19:36:51 +0100

    Core - CHANGELOG.md updated only when GCP content changed

--- src/shell/update_changelog.sh
+++ src/shell/update_changelog.sh
@@ -9,11 +9,6 @@
 git add images/\*
 git add data/\*
 git add exports/\*
-printf -- "\r\n" >> CHANGELOG.md
-printf -- "## $(date '+%Y%m%d-%H%M%S')\r\n" >> CHANGELOG.md
-printf -- "\r\n" >> CHANGELOG.md
-printf -- "### Added\r\n" >> CHANGELOG.md
-printf -- "\r\n" >> CHANGELOG.md
 # Get the list of modifications
 changed_files=$(git diff --name-status HEAD)
 # Count the number of lines in the diff output
@@ -23,6 +18,11 @@ if [ "$line_count" -eq 0 ]; then
   echo "No changes detected."
 else
   echo "Changes detected: $line_count"
+  printf -- "\r\n" >> CHANGELOG.md
+  printf -- "## $(date '+%Y%m%d-%H%M%S')\r\n" >> CHANGELOG.md
+  printf -- "\r\n" >> CHANGELOG.md
+  printf -- "### Added\r\n" >> CHANGELOG.md
+  printf -- "\r\n" >> CHANGELOG.md
   IFS=$'\n'
   for line in $changed_files; do
     status=$(echo "$line" | cut -f1)
